cb1d05c1331c62092da55785fca3115f96df2159
ppupy486 <ppupy486@gmail.com>
2020-09-30 23:32:30 -0400

    Setup New Things

--- app/src/main/java/com/example/decodeit/MainActivity.kt
+++ app/src/main/java/com/example/decodeit/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.decodeit
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        settings_button.setOnClickListener(){
+            val intent = Intent(this, Settings::class.java)
+            println("yeet")
+            startActivity(intent)
+        }
+        start_game.setOnClickListener(){
+            val intent = Intent(this,Codes::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/decodeit/Settings.kt
@@ -0,0 +1,13 @@
+package com.example.decodeit
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.decodeit.R
+
+class Settings: AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.settings)
+    }
+}

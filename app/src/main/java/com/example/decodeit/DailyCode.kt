b4c5012cb5763e9d6a0f3b820a650b174ecb5521
ppupy486 <ppupy486@gmail.com>
2020-10-02 23:43:46 -0400

    Added letter functionality thing

--- app/src/main/java/com/example/decodeit/DailyCode.kt
+++ app/src/main/java/com/example/decodeit/DailyCode.kt
@@ -1,13 +1,22 @@
 package com.example.decodeit
 
+import android.app.Activity
 import android.app.DownloadManager
+import android.content.Context
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.doAfterTextChanged
 import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import kotlinx.android.synthetic.main.daily_code.*
+import kotlinx.android.synthetic.main.daily_code.view.*
 
 class DailyCode: AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,7 +24,18 @@ class DailyCode: AppCompatActivity(){
         setContentView(R.layout.daily_code)
         setDaily()
 
-        daily_submit.setOnClickListener { submit() }
+        daily_input.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+                val imm = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+                imm.hideSoftInputFromWindow(currentFocus!!.windowToken, 0)
+                submit()
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        })
     }
 
     private fun setDaily(){

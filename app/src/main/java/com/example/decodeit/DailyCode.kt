7ba1ed40b1b1f08fcb06f93911cd35e45234d2f8
Sean Lange <42986319+SpicySpices@users.noreply.github.com>
2020-10-04 21:53:11 -0400

    Created Characterized Codes
    Took the 1 singular input from daily code and make an input for each character in the code.

--- app/src/main/java/com/example/decodeit/DailyCode.kt
+++ app/src/main/java/com/example/decodeit/DailyCode.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import com.android.volley.Request
 import com.android.volley.Response
@@ -14,43 +15,30 @@ import kotlinx.android.synthetic.main.daily_code.*
 
 class DailyCode: AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
-        var inside_keyboard = true
         super.onCreate(savedInstanceState)
         setContentView(R.layout.daily_code)
-        setDaily()
 
-        daily_input.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                if (inside_keyboard) {
-                    val imm = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
-                    imm.hideSoftInputFromWindow(currentFocus!!.windowToken, 0)
-                    submit()
-
-                } else {
-                    inside_keyboard = true
-                }
-            }
-
-            override fun afterTextChanged(s: Editable?) {}
-        })
-        daily_input.setOnClickListener(){
-            inside_keyboard = false
-            daily_input.text.clear()
-
-
-        }
+        getDaily()
     }
 
-    private fun setDaily(){
+    private fun getDaily(){
         val queue = Volley.newRequestQueue(this);
         val url = "https://decode-it-server.spicedspices.repl.co/daily/get"
 
         val strReq = StringRequest(
             Request.Method.GET, url,
             Response.Listener<String> { response ->
-                daily_code_code.text = response
+                if(response.length <= 22) {
+                    for (i in response.indices) {
+                        val inputId = resources.getIdentifier("daily_input_$i", "id", packageName)
+                        println(inputId)
+                        val currInp: EditText = findViewById(inputId)
+                        currInp.text =
+                            Editable.Factory.getInstance().newEditable(response[i].toString())
+                    }
+                } else {
+                    println("lol idk the length of the code was longer than number of chars")
+                }
             },
             Response.ErrorListener { println("there was an error lol this was ethan's fault") }
         )
@@ -60,7 +48,7 @@ class DailyCode: AppCompatActivity(){
 
     private fun submit(){
         val queue = Volley.newRequestQueue(this);
-        val userText = daily_input.text
+        val userText = daily_input_0.text
         val url = "https://decode-it-server.spicedspices.repl.co/daily/answer/$userText"
 
         val strReq = StringRequest(
